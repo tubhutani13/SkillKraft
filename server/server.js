013771f235b67f11d05fb1ed2c0745f0376d51b6
Tushar Bhutani <tubhutani13@gmail.com>
2024-02-11 19:26:42 +0530

    added mongodb

--- server/server.js
+++ server/server.js
@@ -1,11 +1,22 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const port = 3000;
+const mongodbConnection = require("./config/mongodbConnection");
 
 app.get("/", (req, res) => {
   res.send("Welcome to SkillKraft");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+mongodbConnection
+  .run()
+  .then(() => {
+    // MongoDB connection is established, now start the server
+    app.listen(port, () => {
+      console.log(`SkillKraft app listening on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to establish MongoDB connection:", err);
+  });
